1a257b9f83059c335ebbe1180eb1a164c7f02e88
kvinkel <43136038+kvinkel@users.noreply.github.com>
2020-04-08 14:24:41 +0200

    Create home screen layout and implement event listeners

--- app/src/main/java/com/example/handytune/HomeActivity.java
+++ app/src/main/java/com/example/handytune/HomeActivity.java
@@ -2,7 +2,10 @@ package com.example.handytune;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +13,43 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.searchButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToSearch();
+            }
+        });
+
+        findViewById(R.id.playlistButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToPlaylist();
+            }
+        });
+
+        findViewById(R.id.userButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToSpotifyUser();
+            }
+        });
+
     }
+
+    public void goToSearch() {
+        Intent intent = new Intent(this, SearchActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToPlaylist() {
+        Intent intent = new Intent(this, PlaylistActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToSpotifyUser() {
+        Intent intent = new Intent(this, SpotifyUserActivity.class);
+        startActivity(intent);
+    }
+
 }
