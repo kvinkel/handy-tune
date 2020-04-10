45f3e4391c17570f0bf0431372a0d8595c103a9c
JakobKaae <jakaa18@student.sdu.dk>
2020-04-10 16:46:41 +0200

    Added onCreate method
    Trying to map elements to functional method usage

--- app/src/main/java/com/example/handytune/SpotifyUserActivity.java
+++ app/src/main/java/com/example/handytune/SpotifyUserActivity.java
@@ -1,4 +1,30 @@
 package com.example.handytune;
 
-public class SpotifyUserActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SpotifyUserActivity extends AppCompatActivity {
+
+    Intent userSearchIntent = new Intent(this, SpotifyUserActivity.class);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_user);
+
+
+
+        findViewById(R.id.userSearchField).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                searchUser();
+            }
+        });
+    }
+
+    public void searchUser () {
+
+    }
 }
