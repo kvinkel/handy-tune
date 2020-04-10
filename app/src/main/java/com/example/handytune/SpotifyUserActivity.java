94907b8dc4a35fcefdbfc52bdfb54a61c9afc20b
JakobKaae <jakaa18@student.sdu.dk>
2020-04-10 17:06:26 +0200

    Removed faulty and unused code

--- app/src/main/java/com/example/handytune/SpotifyUserActivity.java
+++ app/src/main/java/com/example/handytune/SpotifyUserActivity.java
@@ -8,20 +8,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class SpotifyUserActivity extends AppCompatActivity {
 
-    Intent userSearchIntent = new Intent(this, SpotifyUserActivity.class);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_user);
-
-
-
-        findViewById(R.id.userSearchField).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                searchUser();
-            }
-        });
+        
     }
 
     public void searchUser () {
