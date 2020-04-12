dd5b68a1180276ad35cd72ade9dda610802e58b1
frederiktt <32263257+frederiktt@users.noreply.github.com>
2020-04-12 13:03:50 +0200

    added onCreate in PlaylistActivity.java

--- app/src/main/java/com/example/handytune/PlaylistActivity.java
+++ app/src/main/java/com/example/handytune/PlaylistActivity.java
@@ -9,6 +9,6 @@ public class PlaylistActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_search);
+        setContentView(R.layout.activity_playlist);
     }
 }
