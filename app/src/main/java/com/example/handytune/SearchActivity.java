dd1f84a8bf37827440211a7ad5f47771549c2507
kvinkel <43136038+kvinkel@users.noreply.github.com>
2020-04-14 19:41:03 +0200

    Add RecyclerView and String generator

--- app/src/main/java/com/example/handytune/SearchActivity.java
+++ app/src/main/java/com/example/handytune/SearchActivity.java
@@ -1,14 +1,39 @@
 package com.example.handytune;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.Arrays;
+
 public class SearchActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        recyclerView = (RecyclerView) findViewById(R.id.searchRecyclerView);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        String[] strings = generatePlaceholderStrings(50);
+        adapter = new SearchAdapter(strings);
+        recyclerView.setAdapter(adapter);
+
+    }
+
+    public static String[] generatePlaceholderStrings(int amount) {
+        String[] strings = new String[amount];
+        Arrays.fill(strings, "Handy Tune");
+        return strings;
     }
 }
