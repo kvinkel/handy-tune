bc9008155155e7b1499cb4b024400ceebe23a1cc
frederiktt <32263257+frederiktt@users.noreply.github.com>
2020-04-17 13:51:27 +0200

    Added playlist adapter

---
+++ app/src/main/java/com/example/handytune/PlayListAdapter.java
@@ -0,0 +1,43 @@
+package com.example.handytune;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class PlayListAdapter extends RecyclerView.Adapter<PlayListAdapter.ViewHolder> {
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public TextView textView;
+        public View frameLayout;
+
+        public ViewHolder(View frameLayout, TextView v) {
+            super(frameLayout);
+            textView = v;
+        }
+    }
+}
+
+
+
+
