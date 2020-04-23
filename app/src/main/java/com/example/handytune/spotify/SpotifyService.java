46334657301029c681cbafce9de8e999f4ff3245
kvinkel <43136038+kvinkel@users.noreply.github.com>
2020-04-23 21:23:30 +0200

    Add artists endpoint

--- app/src/main/java/com/example/handytune/spotify/SpotifyService.java
+++ app/src/main/java/com/example/handytune/spotify/SpotifyService.java
@@ -1,4 +1,18 @@
 package com.example.handytune.spotify;
 
+import com.example.handytune.spotify.model.Artists;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+import retrofit2.http.Query;
+
 public interface SpotifyService {
+
+    // Example artist search endpoint ../search?q=queen&type=artist
+    @GET("/search")
+    Call<List<Artists>> artists(@Query("artist") String artist, @Query("type") String type);
+    
 }
