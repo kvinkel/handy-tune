5349ba54c4e6e7247acb20a04515e621b3b33561
kvinkel <43136038+kvinkel@users.noreply.github.com>
2020-05-02 16:21:19 +0200

    Change RetroFit version and update endpoint

--- app/src/main/java/com/example/handytune/spotify/SpotifyService.java
+++ app/src/main/java/com/example/handytune/spotify/SpotifyService.java
@@ -1,18 +1,24 @@
 package com.example.handytune.spotify;
 
 import com.example.handytune.spotify.model.Artists;
+import com.example.handytune.spotify.model.MusicSearchResult;
 
 import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
 
+import retrofit2.http.Headers;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface SpotifyService {
 
     // Example artist search endpoint ../search?q=queen&type=artist
-    @GET("/search")
-    Call<List<Artists>> artists(@Query("artist") String artist, @Query("type") String type);
-    
+
+    // Temporary authorization solution with header
+    @Headers({"Authorization: Bearer "})
+    @GET("search")
+    Call<MusicSearchResult> searchMusic(@Query("q") String query, @Query("type") String searchType);
+
 }
