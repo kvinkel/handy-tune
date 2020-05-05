02177cce48a4ccd197bd945ae158c0c590fe6341
frederiktt <32263257+frederiktt@users.noreply.github.com>
2020-05-05 13:25:16 +0200

    added AppDatabase.java + BaseActivity.java

---
+++ app/src/main/java/com/example/handytune/database/AppDatabase.java
@@ -0,0 +1,48 @@
+package com.example.handytune.database;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.room.DatabaseConfiguration;
+import androidx.room.InvalidationTracker;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteOpenHelper;
+
+public abstract class AppDatabase  extends RoomDatabase {
+
+    private static AppDatabase INSTANCE;
+
+    public abstract AlbumDAO albumDAO();
+    public abstract TrackDAO trackDAO();
+    public abstract PlaylistDAO playlistDAO();
+
+    @NonNull
+    @Override
+    protected SupportSQLiteOpenHelper createOpenHelper(DatabaseConfiguration config) {
+        return null;
+    }
+
+    @NonNull
+    @Override
+    protected InvalidationTracker createInvalidationTracker() {
+        return null;
+    }
+
+    @Override
+    public void clearAllTables() {
+
+    }
+
+    public static AppDatabase getAppDatabase(Context context) {
+        if (INSTANCE == null) {
+            INSTANCE =
+                    Room.databaseBuilder(context.getApplicationContext(), AppDatabase.class, "spotify-database")
+                            // allow queries on the main thread.
+                            // Don't do this on a real app!
+                            .allowMainThreadQueries()
+                            .build();
+        }
+        return INSTANCE;
+    }
+}
