831457fcaa43d79474d128b1c3f3f0ebce34816e
JakobKaae <jakaa18@student.sdu.dk>
2020-05-05 12:41:11 +0200

    Added endpoint

--- app/src/main/java/com/example/handytune/spotify/SpotifyService.java
+++ app/src/main/java/com/example/handytune/spotify/SpotifyService.java
@@ -1,11 +1,13 @@
 package com.example.handytune.spotify;
 
 import com.example.handytune.spotify.model.MusicSearchResult;
+import com.example.handytune.spotify.model.UserSearchResult;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
 
 import retrofit2.http.Headers;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface SpotifyService {
@@ -16,5 +18,7 @@ public interface SpotifyService {
     @Headers({"Authorization: Bearer "})
     @GET("search")
     Call<MusicSearchResult> searchMusic(@Query("q") String query, @Query("type") String searchType);
+    @GET("users/{user_id}")
+    Call<UserSearchResult> searchUser(@Path("user_id") String query);
 
 }
