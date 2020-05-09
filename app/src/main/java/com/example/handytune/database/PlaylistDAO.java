601f6b5b2f0e5191e318ba7a007269e802a8cfee
frederiktt <32263257+frederiktt@users.noreply.github.com>
2020-05-09 18:47:19 +0200

    added query for select from playlist and added changes to PlaylistDAO.java from Playlist

--- app/src/main/java/com/example/handytune/database/PlaylistDAO.java
+++ app/src/main/java/com/example/handytune/database/PlaylistDAO.java
@@ -3,6 +3,7 @@ package com.example.handytune.database;
 import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.Query;
+import androidx.room.Transaction;
 
 import java.util.List;
 
@@ -11,9 +12,13 @@ public interface PlaylistDAO {
     @Insert
     void insertPlaylist(Playlist playlist);
 
-    @Query("SELECT id,userId,playListName from playlist")
+    @Query("SELECT playlistId,playListName from playlist")
     List<Playlist> getAllPLaylists();
 
     @Query("DELETE FROM playlist")
     public void nukePlaylist();
+
+    @Transaction
+    @Query("SELECT * FROM playlist")
+    List<PlaylistWithTracks> getTrackWithPlaylists();
 }
