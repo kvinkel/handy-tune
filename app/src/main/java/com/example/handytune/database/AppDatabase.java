251ba4613cc7746a406e9cc79e9c70e948d6090b
frederiktt <32263257+frederiktt@users.noreply.github.com>
2020-05-09 18:38:38 +0200

    deleted album from AppDatabase.java

--- app/src/main/java/com/example/handytune/database/AppDatabase.java
+++ app/src/main/java/com/example/handytune/database/AppDatabase.java
@@ -3,10 +3,9 @@ package com.example.handytune.database;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {Album.class, Track.class, Playlist.class }, version = 1, exportSchema = false)
+@Database(entities = { Track.class, Playlist.class }, version = 1, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
 
-    public abstract AlbumDAO albumDAO();
     public abstract TrackDAO trackDAO();
     public abstract PlaylistDAO playlistDAO();
 
