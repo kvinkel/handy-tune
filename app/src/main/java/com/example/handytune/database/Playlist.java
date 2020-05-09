bf1b278dd5abf33c254d5f7e81a499269e51a689
frederiktt <32263257+frederiktt@users.noreply.github.com>
2020-05-09 18:44:15 +0200

    chenged id to playlistId and removed userId

--- app/src/main/java/com/example/handytune/database/Playlist.java
+++ app/src/main/java/com/example/handytune/database/Playlist.java
@@ -1,36 +1,23 @@
 package com.example.handytune.database;
-
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
-
 import java.io.Serializable;
 
 @Entity(tableName = "playlist")
 public class Playlist implements Serializable {
     @PrimaryKey(autoGenerate = true)
-    @ColumnInfo(name = "id")
-    private int id;
-    @ColumnInfo(name = "userId")
-    private int userId;
+    @ColumnInfo(name = "playlistId")
+    private int playlistId;
     @ColumnInfo(name = "playlistName")
     private String playlistName;
 
-
-    public int getId() {
-        return id;
+    public int getPlaylistId() {
+        return playlistId;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getUserId() {
-        return userId;
-    }
-
-    public void setUserId(int userId) {
-        this.userId = userId;
+    public void setPlaylistId(int playlistId) {
+        this.playlistId = playlistId;
     }
 
     public String getPlaylistName() {
