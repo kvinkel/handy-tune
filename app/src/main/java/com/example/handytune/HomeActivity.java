54666a650a9a2a5a6947ee2430ce7112c013e6bc
frederiktt <32263257+frederiktt@users.noreply.github.com>
2020-05-09 18:51:04 +0200

    test insert playlist and tracks to Room

--- app/src/main/java/com/example/handytune/HomeActivity.java
+++ app/src/main/java/com/example/handytune/HomeActivity.java
@@ -4,11 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 
-import java.util.ArrayList;
+import com.example.handytune.database.Playlist;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -52,6 +50,15 @@ public class HomeActivity extends AppCompatActivity {
         startThreadForInsertPlayLists(2,"Kims Playlist");
         startThreadForInsertPlayLists(3,"Frederiks Playlist");
 
+        startThreadForInsertTracks(1,"TestTrack 1", "TestExternalTrackUrl", "TestOpenInAppTrackUrl", "TestAlbumUrl","Jakobs Playlist");
+        startThreadForInsertTracks(2,"TestTrack 2", "TestExternalTrackUrl", "TestOpenInAppTrackUrl", "TestAlbumUrl","Kims Playlist");
+        startThreadForInsertTracks(3,"TestTrack 3", "TestExternalTrackUrl", "TestOpenInAppTrackUrl", "TestAlbumUrl", "Frederiks Playlist");
+        startThreadForInsertTracks(4,"TestTrack 4", "TestExternalTrackUrl", "TestOpenInAppTrackUrl", "TestAlbumUrl","Kims Playlist");
+        startThreadForInsertTracks(5,"TestTrack 4", "TestExternalTrackUrl", "TestOpenInAppTrackUrl", "TestAlbumUrl","Kims Playlist");
+        startThreadForInsertTracks(6,"TestTrack 4", "TestExternalTrackUrl", "TestOpenInAppTrackUrl", "TestAlbumUrl","Kims Playlist");
+        startThreadForInsertTracks(7,"TestTrack 4", "TestExternalTrackUrl", "TestOpenInAppTrackUrl", "TestAlbumUrl","Kims Playlist");
+
+
     }
 
     @Override
@@ -76,42 +83,30 @@ public class HomeActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-
-    public void startThreadForInsertPlayLists(final int userId,final String playListName) {
+//TODO For testing**********************************************************************************
+    public void startThreadForInsertPlayLists(final int playlistId,final String playListName) {
         //Create a thread
         insertThread = new Thread(new Runnable() {
             @Override
             public void run() {
-                /*For testing*/
-                dbRepository.insertPlaylist(userId, playListName);
-                System.out.println("Inserted: \n User id: " + userId + "\n Playlist name: " + playListName );
+                dbRepository.insertPlaylist(playlistId, playListName);
             }
         });
         insertThread.start();
     }
 
-    public void startThreadForInsertTracks(final int userId,final String playListName) {
+    public void startThreadForInsertTracks(final int trackId, final String trackName, final String externalTrackUrl, final String openInAppTrackUrl, final String albumImageUrl, final String belongToPlaylistName  ) {
         //Create a thread
         insertThread = new Thread(new Runnable() {
             @Override
             public void run() {
+              dbRepository.insertTrack(trackId,trackName,externalTrackUrl,openInAppTrackUrl,albumImageUrl,belongToPlaylistName);
 
             }
         });
         insertThread.start();
     }
 
-    public void startThreadForInsertAlbum() {
-
-        //Create a thread
-        insertThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-
-            }
-        });
-        insertThread.start();
-    }
 
     public void startThreadForDeleteDataInDatabase() {
 
@@ -119,9 +114,9 @@ public class HomeActivity extends AppCompatActivity {
         deleteThread = new Thread(new Runnable() {
             @Override
             public void run() {
-                System.out.println("Nuke Albums & Playlists");
-                dbRepository.nukeAlbum();
-                dbRepository.nukePlaylist();
+                System.out.println("Playlists and Track");
+                dbRepository.nukePlaylistInDatabase();
+                dbRepository.nukeTracksInDatabase();
             }
         });
         deleteThread.start();
@@ -132,5 +127,7 @@ public class HomeActivity extends AppCompatActivity {
         super.onDestroy();
         insertThread.interrupt();
         deleteThread.interrupt();
+
+        dbRepository = null;
     }
 }
