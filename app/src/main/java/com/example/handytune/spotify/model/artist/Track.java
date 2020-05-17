1b7872455f8b0458374ced6481c6532bf1d759f1
kvinkel <43136038+kvinkel@users.noreply.github.com>
2020-05-17 21:38:33 +0200

    Add models for artists endpoint

---
+++ app/src/main/java/com/example/handytune/spotify/model/artist/Track.java
@@ -0,0 +1,81 @@
+package com.example.handytune.spotify.model.artist;
+
+import java.util.List;
+
+import com.example.handytune.spotify.model.ExternalUrls;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Track {
+    @SerializedName("album")
+    @Expose
+    private Album album;
+    @SerializedName("external_urls")
+    @Expose
+    private ExternalUrls externalUrls;
+    @SerializedName("id")
+    @Expose
+    private String id;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("track_number")
+    @Expose
+    private Integer trackNumber;
+    @SerializedName("type")
+    @Expose
+    private String type;
+    @SerializedName("uri")
+    @Expose
+    private String uri;
+
+    public ExternalUrls getExternalUrls() {
+        return externalUrls;
+    }
+
+    public void setExternalUrls(ExternalUrls externalUrls) {
+        this.externalUrls = externalUrls;
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getTrackNumber() {
+        return trackNumber;
+    }
+
+    public void setTrackNumber(Integer trackNumber) {
+        this.trackNumber = trackNumber;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getUri() {
+        return uri;
+    }
+
+    public void setUri(String uri) {
+        this.uri = uri;
+    }
+
+}
