d84efcc5c410a6c7f22a138c5d94d32085d6880e
kvinkel <43136038+kvinkel@users.noreply.github.com>
2020-05-17 22:29:57 +0200

    Add topTracks endpoint

--- app/src/main/java/com/example/handytune/spotify/SpotifyService.java
+++ app/src/main/java/com/example/handytune/spotify/SpotifyService.java
@@ -2,12 +2,12 @@ package com.example.handytune.spotify;
 
 import com.example.handytune.spotify.model.MusicSearchResult;
 import com.example.handytune.spotify.model.UserSearchResult;
+import com.example.handytune.spotify.model.artist.TopTracks;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
 
 import retrofit2.http.Header;
-import retrofit2.http.Headers;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 
@@ -17,6 +17,8 @@ public interface SpotifyService {
     @GET("search")
     Call<MusicSearchResult> searchMusic(@Query("q") String query, @Query("type") String searchType, @Header("Authorization") String token);
     @GET("users/{user_id}")
-    Call<UserSearchResult> searchUser(@Path("user_id") String query, @Header("Authorization") String token);
+    Call<UserSearchResult> searchUser(@Path("user_id") String userId, @Header("Authorization") String token);
+    @GET("artists/{artist_id}/top-tracks")
+    Call<TopTracks> topTracks(@Path("artist_id") String artistId, @Query("country") String country, @Header("Authorization") String token);
 
 }
