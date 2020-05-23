11e04f1cad78585ebd8e46f5838681645baa5145
kvinkel <43136038+kvinkel@users.noreply.github.com>
2020-05-23 16:58:49 +0200

    Add TrackFragment transaction in ResultActivity

--- app/src/main/java/com/example/handytune/ResultActivity.java
+++ app/src/main/java/com/example/handytune/ResultActivity.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
 
 import com.example.handytune.fragments.ArtistFragment;
+import com.example.handytune.fragments.TrackFragment;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -21,15 +22,19 @@ public class ResultActivity extends AppCompatActivity {
 
         switch (resultType) {
             case SearchActivity.ResultTypes.ARTIST:
-                String itemId = getIntent().getExtras().getString(SearchActivity.ResultTypes.ITEM_ID);
+                String artistId = getIntent().getExtras().getString(SearchActivity.ResultTypes.ITEM_ID);
                 String artistName = getIntent().getExtras().getString(SearchActivity.ResultTypes.NAME);
-                String artistImageUrl = getIntent().getExtras().getString(SearchActivity.ResultTypes.ARTIST_IMAGE_URL);
-                ArtistFragment fragment = ArtistFragment.newInstance(itemId, artistName, artistImageUrl);
-                transaction.add(R.id.artistFrame, fragment);
+                String artistImageUrl = getIntent().getExtras().getString(SearchActivity.ResultTypes.IMAGE_URL);
+                ArtistFragment artistFragment = ArtistFragment.newInstance(artistId, artistName, artistImageUrl);
+                transaction.add(R.id.frame, artistFragment);
                 transaction.commit();
                 break;
             case SearchActivity.ResultTypes.ALBUM:
-                // TODO Go to track fragment
+                String albumId = getIntent().getExtras().getString(SearchActivity.ResultTypes.ITEM_ID);
+                String albumImageUrl = getIntent().getExtras().getString(SearchActivity.ResultTypes.IMAGE_URL);
+                TrackFragment trackFragment = TrackFragment.newInstance(albumId, albumImageUrl);
+                transaction.add(R.id.frame, trackFragment);
+                transaction.commit();
                 break;
             case SearchActivity.ResultTypes.TRACK:
                 // TODO Go to playlist fragment
