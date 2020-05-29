ba175e5483c2127cec9ee2364f8fa5da04b5bb6e
kvinkel <43136038+kvinkel@users.noreply.github.com>
2020-05-29 17:13:29 +0200

    Add check for savedInstance in ResultActivity

--- app/src/main/java/com/example/handytune/ResultActivity.java
+++ app/src/main/java/com/example/handytune/ResultActivity.java
@@ -21,26 +21,29 @@ public class ResultActivity extends AppCompatActivity {
         FragmentTransaction transaction = manager.beginTransaction();
 
 
-
         switch (resultType) {
             case SearchActivity.ResultTypes.ARTIST:
                 String artistId = getIntent().getExtras().getString(SearchActivity.ResultTypes.ITEM_ID);
                 String artistName = getIntent().getExtras().getString(SearchActivity.ResultTypes.NAME);
                 String artistImageUrl = getIntent().getExtras().getString(SearchActivity.ResultTypes.IMAGE_URL);
-                ArtistFragment artistFragment = ArtistFragment.newInstance(artistId, artistName, artistImageUrl);
-                if (findViewById(R.id.frame1) != null) { // check for tablet layout
-                    transaction.add(R.id.frame1, artistFragment);
-                } else {
-                    transaction.add(R.id.frame, artistFragment);
+                if (savedInstanceState == null) {
+                    ArtistFragment artistFragment = ArtistFragment.newInstance(artistId, artistName, artistImageUrl);
+                    if (findViewById(R.id.frame1) != null) { // check for tablet layout
+                        transaction.add(R.id.frame1, artistFragment);
+                    } else {
+                        transaction.add(R.id.frame, artistFragment);
+                    }
+                    transaction.commit();
                 }
-                transaction.commit();
                 break;
             case SearchActivity.ResultTypes.ALBUM:
                 String albumId = getIntent().getExtras().getString(SearchActivity.ResultTypes.ITEM_ID);
                 String albumImageUrl = getIntent().getExtras().getString(SearchActivity.ResultTypes.IMAGE_URL);
-                TrackFragment trackFragment = TrackFragment.newInstance(albumId, albumImageUrl);
-                transaction.add(R.id.frame, trackFragment);
-                transaction.commit();
+                if (savedInstanceState == null) {
+                    TrackFragment trackFragment = TrackFragment.newInstance(albumId, albumImageUrl);
+                    transaction.add(R.id.frame, trackFragment);
+                    transaction.commit();
+                }
                 break;
             case SearchActivity.ResultTypes.TRACK:
                 // TODO Go to playlist fragment
