64c7e5c7dd308f23c5d1be1bae47e01238714ec5
frederiktt <32263257+frederiktt@users.noreply.github.com>
2020-05-29 21:00:10 +0200

    added save playlist to CreatePlaylistFragment

--- app/src/main/java/com/example/handytune/fragments/CreatePlaylistFragment.java
+++ app/src/main/java/com/example/handytune/fragments/CreatePlaylistFragment.java
@@ -1,21 +1,29 @@
 package com.example.handytune.fragments;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.example.handytune.DbRepository;
 import com.example.handytune.R;
+import com.example.handytune.ResultActivity;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link CreatePlaylistFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class CreatePlaylistFragment extends Fragment {
+public class CreatePlaylistFragment extends Fragment implements View.OnClickListener {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -24,6 +32,9 @@ public class CreatePlaylistFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private TextView textView;
+    Button savePlaylistBtn;
+    DbRepository dbRepository;
 
     public CreatePlaylistFragment() {
         // Required empty public constructor
@@ -48,15 +59,39 @@ public class CreatePlaylistFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+
         }
+        dbRepository = new DbRepository(getContext());
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_playlist, container, false);
+        ConstraintLayout layout = (ConstraintLayout) inflater.inflate(R.layout.fragment_create_playlist, container, false);
+        textView = (TextView) layout.getViewById(R.id.createPlaylist);
+        savePlaylistBtn = (Button) layout.findViewById(R.id.savePlaylistBtn);
+        savePlaylistBtn.setOnClickListener(this::onClick);
+
+        return layout;
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.savePlaylistBtn:
+                String name = textView.getText().toString();
+                dbRepository.insertPlaylist(name);
+
+                ResultActivity resultActivity = (ResultActivity) getActivity();
+                resultActivity.updateFragment();
+
+                break;
+
+
+        }
+    }
+
+
 }
