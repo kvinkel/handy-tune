4adf4e96bf09ab23952f1f1809574cd16c724b3e
frederiktt <32263257+frederiktt@users.noreply.github.com>
2020-05-29 20:47:42 +0200

    removed sout and added setter for playlistWithTracks + cleanup

--- app/src/main/java/com/example/handytune/AddToPlaylistAdapter.java
+++ app/src/main/java/com/example/handytune/AddToPlaylistAdapter.java
@@ -1,7 +1,6 @@
 package com.example.handytune;
 
 import android.content.Context;
-import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,21 +8,18 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.handytune.database.PlaylistWithTracks;
 import com.example.handytune.database.Track;
-import com.example.handytune.fragments.CreatePlaylistFragment;
+
 
 import java.util.ArrayList;
 
 public class AddToPlaylistAdapter extends RecyclerView.Adapter<AddToPlaylistAdapter.PlaylistViewHolder> {
 
 
-    ArrayList<PlaylistWithTracks> playlistWithTracks;
+    private ArrayList<PlaylistWithTracks> playlistWithTracks;
 
     DbRepository dbRepository;
     Track track;
@@ -61,7 +57,6 @@ public class AddToPlaylistAdapter extends RecyclerView.Adapter<AddToPlaylistAdap
     public static class PlaylistViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         // each data item is just a string in this case
         private TextView textView;
-        private View frameLayout;
         private Context context;
         ArrayList<PlaylistWithTracks> playlistWithTracks;
         DbRepository dbRepository;
@@ -76,21 +71,6 @@ public class AddToPlaylistAdapter extends RecyclerView.Adapter<AddToPlaylistAdap
             this.dbRepository = dbRepository;
             this.track = track;
             this.context = context;
-
-//            createPlaylistBtn.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//
-//                    System.out.println("##################################");
-//                    //call fragment
-//                    FragmentManager manager = ((AppCompatActivity) context).getSupportFragmentManager();
-//                    FragmentTransaction transaction = manager.beginTransaction();
-//                    CreatePlaylistFragment createPlaylistFragment= CreatePlaylistFragment.newInstance();
-//                    transaction.add(R.id.artistFrame, createPlaylistFragment);
-//                    transaction.commit();
-//                }
-//            });
-
         }
 
         public TextView getTextView() {
@@ -102,24 +82,14 @@ public class AddToPlaylistAdapter extends RecyclerView.Adapter<AddToPlaylistAdap
 
             int position = getAdapterPosition();
             String rowName = playlistWithTracks.get(position).playlist.getPlaylistName();
-            System.out.println("Clicked on position : " + position + " ******************");
-            System.out.println("Clicked on name : " + rowName + " ******************");
-            System.out.printf("Track name: " + track.getTrackName());
-            System.out.println("Image url: " + track.getAlbumImageUrl());
-
             track.setBelongToPlaylistName(rowName);
-            track.setTrackName("Test from adapter");
-            track.setAlbumImageUrl("test album url from adapter");
-            track.setExternalTrackUrl("test external url from adapter");
-            track.setOpenInAppTrackUrl("test open in app url from adapter");
-            track.setTrackId(1000);
             dbRepository.insertTrack(track);
-
-
         }
-
     }
-
+    
+    public void setPlaylistWithTracks(ArrayList<PlaylistWithTracks> playlistWithTracks) {
+        this.playlistWithTracks = playlistWithTracks;
+    }
 }
 
 
