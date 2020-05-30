82e7b0b4b8dbd950fad6aa298874976a6d9f7369
JakobKaae <jakaa18@student.sdu.dk>
2020-05-30 17:32:44 +0200

    Created onClick to TrackFragment

--- app/src/main/java/com/example/handytune/AlbumAdapter.java
+++ app/src/main/java/com/example/handytune/AlbumAdapter.java
@@ -8,9 +8,12 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.handytune.fragments.TrackFragment;
 import com.example.handytune.spotify.model.Item;
 
 import java.util.List;
@@ -39,6 +42,8 @@ public class AlbumAdapter extends RecyclerView.Adapter<AlbumAdapter.AlbumViewHol
         holder.getAlbumListView().setText(result.get(position).getName());
         Glide.with(context).clear(holder.getAlbumImage());
         Glide.with(context).load(result.get(position).getImages().get(0).getUrl()).placeholder(R.drawable.music_note).into(holder.albumImage);
+        holder.albumId = result.get(position).getId();
+        holder.imageUrl = result.get(position).getImages().get(0).getUrl();
     }
 
     @Override
@@ -50,16 +55,39 @@ public class AlbumAdapter extends RecyclerView.Adapter<AlbumAdapter.AlbumViewHol
         return result.get(position).getName();
     }
 
+    public String getAlbumId() {return result.get(0).getId();}
+    public String getImageUrl() {return result.get(0).getImages().get(0).getUrl();}
+
     public static class AlbumViewHolder extends RecyclerView.ViewHolder {
         private Context context;
         private TextView textView;
         private ImageView albumImage;
+        private String albumId;
+        private String imageUrl;
 
-        public AlbumViewHolder(@NonNull Context context,  View itemView, TextView textView, ImageView albumImage) {
+        public AlbumViewHolder(@NonNull Context context, View itemView, TextView textView, ImageView albumImage) {
             super(itemView);
             this.textView = textView;
             this.albumImage = albumImage;
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    FragmentManager manager = ((ResultActivity)context).getSupportFragmentManager();
+                    FragmentTransaction transaction = manager.beginTransaction();
+                    TrackFragment fragment = TrackFragment.newInstance(getAlbumId(), getImageUrl());
+                    transaction.replace(R.id.frame, fragment).addToBackStack(null);
+                    transaction.commit();
+                }
+            });
         }
+        public String getImageUrl() { return imageUrl; }
+
+        public void setImageUrl(String imageUrl) { this.imageUrl = imageUrl; }
+
+        public String getAlbumId() { return albumId; }
+
+        public void setAlbumId(String albumId) { this.albumId = albumId; }
         public TextView getAlbumListView() {return textView;}
         public ImageView getAlbumImage() {return albumImage;}
     }
