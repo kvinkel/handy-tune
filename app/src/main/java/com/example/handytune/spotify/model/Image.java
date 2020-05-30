dbb1b4d04f0176b6c1d25885dcf02dd45668de9d
JakobKaae <jakaa18@student.sdu.dk>
2020-05-30 17:41:33 +0200

    Implement Serializeable to avoid exception

--- app/src/main/java/com/example/handytune/spotify/model/Image.java
+++ app/src/main/java/com/example/handytune/spotify/model/Image.java
@@ -3,7 +3,9 @@ package com.example.handytune.spotify.model;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
-public class Image {
+import java.io.Serializable;
+
+public class Image implements Serializable {
 
     @SerializedName("height")
     @Expose
--- app/src/main/java/com/example/handytune/spotify/model/Item.java
+++ app/src/main/java/com/example/handytune/spotify/model/Item.java
@@ -3,9 +3,10 @@ package com.example.handytune.spotify.model;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
+import java.io.Serializable;
 import java.util.List;
 
-public class Item {
+public class Item implements Serializable {
 
     @SerializedName("collaborative")
     @Expose
